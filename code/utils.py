90ddf4c5bc463330a2426e46ce2b6e2ae0327e84
Andreas Lordos <andreasglordos@gmail.com>
2018-09-16 01:01:01 +0300

    Added utils.py

---
+++ code/utils.py
@@ -0,0 +1,6 @@
+def removeDuplicates(nodes):
+    '''
+    Input: nodes of events with lat and long as well as dates. If date, lat and long
+    are all similar, then this is a duplicate event
+    '''
+    
